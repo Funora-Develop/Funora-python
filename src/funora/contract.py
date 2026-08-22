00b0ed8c70ac17bc18bdb255bf68f5e31060e5c4
coder-smookki <medved3loy@yandex.ru>
2026-08-22 13:03:47 +0300

    порождённая версия контракта 0.2.0

--- src/funora/contract.py
+++ src/funora/contract.py
@@ -28,7 +28,7 @@ __all__ = [
 ]
 
 #: Версия спецификации, которую реализует пакет.
-SPEC_VERSION: Final[str] = "0.1.0"
+SPEC_VERSION: Final[str] = "0.2.0"
 
 #: Состояние спецификации: draft либо released.
 #:
