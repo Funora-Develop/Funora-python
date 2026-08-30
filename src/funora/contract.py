527cdefca24611498f0087d770ed5b5f78ff11b3
coder-smookki <medved3loy@yandex.ru>
2026-08-30 23:48:23 +0300

    Имя метода и имена полей формы читаются в скелете

--- src/funora/contract.py
+++ src/funora/contract.py
@@ -28,7 +28,7 @@ __all__ = [
 ]
 
 #: Версия спецификации, которую реализует пакет.
-SPEC_VERSION: Final[str] = "0.19.0"
+SPEC_VERSION: Final[str] = "0.20.0"
 
 #: Состояние спецификации: draft либо released.
 #:
