889ebbe31ff568299f1ba4a303c3fd9ef26e2cd6
coder-smookki <medved3loy@yandex.ru>
2026-08-31 04:14:31 +0300

    Ответ 404 перестал занимать имя снимка страницы

--- src/funora/contract.py
+++ src/funora/contract.py
@@ -28,7 +28,7 @@ __all__ = [
 ]
 
 #: Версия спецификации, которую реализует пакет.
-SPEC_VERSION: Final[str] = "0.24.0"
+SPEC_VERSION: Final[str] = "0.25.0"
 
 #: Состояние спецификации: draft либо released.
 #:
